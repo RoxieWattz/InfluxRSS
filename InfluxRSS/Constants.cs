c4fb4e6050f708d1b2ce24bc6785cf7899a2c338
Roxie Wattz <roxie.wattz@gmail.com>
2022-10-08 12:18:14 -0700

    Release prep: Version constants

---
+++ InfluxRSS/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfluxRSS
+{
+	public static class Constants
+	{
+		public const string VERSION = "0.1.0";
+		public const long VERSION_NUMBER = 1;
+	}
+}
